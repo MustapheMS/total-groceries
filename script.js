1ffd4ee08450d8fb278bd0134faeb2e2ce22335c
MustapheMS <moztaphem9@gmail.com>
2023-06-13 22:10:28 +0300

    Groceries Done

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Good Luck 💪🏾
+
+//Done 💯💯
+function getTotalPrice(grocery){
+    let total = 0;
+
+    for(let i=0; i<grocery.length; i++){
+        total = total + grocery[i].quantity*grocery[i].price;
+    }
+
+    return total;
+}
+
+
+
+  console.log(getTotalPrice([
+    { product: "Milk", quantity: 5, price: 1.50 },
+    { product: "Cereals", quantity: 1, price: 2.50 },
+    { product: "Bread", quantity: 1, price: 3.19 },
+    { product: "EGG", quantity: 1, price: 2.50 }
+  ]))
